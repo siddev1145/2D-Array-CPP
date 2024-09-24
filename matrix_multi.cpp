7e1ef88f5152c3e484f98748e2f57a1a7458e829
Siddhant Bedre <siddhantbedre2004@gmail.com>
2024-09-25 00:19:49 +0530

    Create matrix_multi.cpp

---
+++ matrix_multi.cpp
@@ -0,0 +1,93 @@
+//Name: Siddhant Bedre
+//PRN: 23070123037
+//Aim: Using 2D arrays for matrix multiplicatiion
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r1, c1, r2, c2;
+
+    cout << "Enter the number of rows of the first matrix: ";
+    cin >> r1;
+    cout << "Enter the number of columns of the first matrix: ";
+    cin >> c1;
+
+    cout << "Enter the number of rows of the second matrix: ";
+    cin >> r2;
+    cout << "Enter the number of columns of the second matrix: ";
+    cin >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication is not possible. Number of columns in the first matrix must equal the number of rows in the second matrix." << endl;
+        return 1;
+    }
+
+    int A[r1][c1], B[r2][c2], C[r1][c2] = {0};
+
+    cout << "Enter elements of the first matrix:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cout << "A[" << i << "][" << j << "]: ";
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of the second matrix:" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << "B[" << i << "][" << j << "]: ";
+            cin >> B[i][j];
+        }
+    }
+
+    
+    cout << "Matrix A:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    
+    cout << "Matrix B:" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            for (int k = 0; k < c1; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    // Print result matrix C
+    cout << "The resultant matrix is:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/* Output-
+
+A matrix: 
+1 2 
+4 5 
+B matrix: 
+7 8 
+9 10 
+Result matrix C:
+58 64 
+139 154 
+*/
